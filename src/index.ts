d188f2a28ba81235eb9f2d119bac7fa946cd9400
balinux <ryojuniyantara@gmail.com>
2026-07-08 19:12:14 +0800

    fix: endpoint api schedule

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 import { initDb } from "./db/schema";
-import { printUpcomingMedicines } from "./services/reminder";
+import {
+  getUpcomingMedicines,
+  printUpcomingMedicines,
+} from "./services/reminder";
 import { Hono } from "hono";
 
 // Inisialisasi Database jika belum ada
 initDb();
 
-console.log("Medibox Bun is running...");
-printUpcomingMedicines();
+// console.log("Medibox Bun is running...");
+// printUpcomingMedicines();
 
 const app = new Hono();
 // 1. GET ALL BOOKS (Read)
 app.get("/api/schedule", (c) => {
-  return c.json({
-    id: 11,
-    medicine_name: "Amoxicillin",
-    schedule_time: "13:45",
-  });
+  const medicines = getUpcomingMedicines();
+  return c.json(medicines);
 });
 
 export default app;
